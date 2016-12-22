e22233cb5d10caf8a260636705409d4b5d314ca6
mohseenrm <mohseenmukaddam6@gmail.com>
2016-12-22 13:31:23 -0700

    updated packages

--- extension.js
+++ extension.js
@@ -18,7 +18,7 @@ function activate(context) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    var disposable = vscode.commands.registerCommand('extension.sayHello', function () {
+    var disposable = vscode.commands.registerCommand('extension.litIt', function () {
         
         // doc reference
         // https://code.visualstudio.com/Docs/extensionAPI/vscode-api#Range
