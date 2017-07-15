d01f6690acb1f5e08d423f6c4354da7a2b951851
unknown <klamse47@gmail.com>
2017-07-14 22:17:59 -0500

    Add typescript annotations and force you to use them :)

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -19,10 +19,10 @@ import {
 suite('Doc String generation', () => {
 
     test('standard function signature check', () => {
-        const functionSignature = ' function testFn(x, y){';
-        const functionSignature2 = 'export default function testFn(x, y)';
-        const functionSignature3 = 'const beta = function testFn()';
-        const functionSignature4 = 'var x = function testFn(y){';
+        const functionSignature: string = ' function testFn(x, y){';
+        const functionSignature2: string = 'export default function testFn(x, y)';
+        const functionSignature3: string = 'const beta = function testFn()';
+        const functionSignature4: string = 'var x = function testFn(y){';
 
         expect( checkSignature( functionSignature ) ).to.be.equal( 'FUNCTION' );
         expect( checkSignature( functionSignature2 ) ).to.be.equal( 'FUNCTION' );
@@ -31,11 +31,11 @@ suite('Doc String generation', () => {
     });
 
     test('ES6 function signature check', () => {
-        const signature = '() => {';
-        const signature2 = '( velocity ) => {';
-        const signature3 = '( velocity, distance ) => {';
-        const signature4 = 'const testES6 = ( x ) => { return x+1 };';
-        const signature5 = 'let x = (x) => x*3;';
+        const signature: string = '() => {';
+        const signature2: string = '( velocity ) => {';
+        const signature3: string = '( velocity, distance ) => {';
+        const signature4: string = 'const testES6 = ( x ) => { return x+1 };';
+        const signature5: string = 'let x = (x) => x*3;';
 
         expect( checkSignature( signature ) ).to.be.equal( 'ES6' );
         expect( checkSignature( signature2 ) ).to.be.equal( 'ES6' );
@@ -45,10 +45,10 @@ suite('Doc String generation', () => {
     });
 
     test('check parameters', () => {
-        const signature = ' function fn1(){';
-        const signature2 = ' function fn2(a, b, c)';
-        const signature3 = ' () = {console.log();}';
-        const signature4 = ' (x, y, z)=>{return x+y+z;}';
+        const signature: string = ' function fn1(){';
+        const signature2: string = ' function fn2(a, b, c)';
+        const signature3: string = ' () = {console.log();}';
+        const signature4: string = ' (x, y, z)=>{return x+y+z;}';
 
         expect( extractParameters( signature ) ).to.deep.equal( [] );
         expect( extractParameters( signature2 ) ).to.deep.equal( ['a', 'b', 'c'] );
@@ -57,10 +57,10 @@ suite('Doc String generation', () => {
     });
 
     test('pretty formatting', () => {
-        const signature = [];
-        const signature2 = ['param1', 'param', 'xz', 'x'];
-        const signature3 = ['xyz'];
-        const signature4 = ['first_parameter', 'a', 'param2'];
+        const signature: string[] = [];
+        const signature2: string[] = ['param1', 'param', 'xz', 'x'];
+        const signature3: string[] = ['xyz'];
+        const signature4: string[] = ['first_parameter', 'a', 'param2'];
 
         expect( prettyParameters( signature ) ).to.deep.equal( [] );
         expect( prettyParameters( signature2 ) ).to.deep.equal( ['param1', 'param ', 'xz    ', 'x     '] );
@@ -69,12 +69,12 @@ suite('Doc String generation', () => {
     });
 
     test('extracting function name', () => {
-        const signature = 'const customFn = () => {}';
-        const signature2 = 'let customFn = () => {}';
-        const signature3 = 'var customFn = function(x) => {';
-        const signature4 = 'function customFn(x, y){';
-        const signature5 = 'function customFn(){';
-        const signature6 = 'let x = function(param){';
+        const signature: string = 'const customFn = () => {}';
+        const signature2: string = 'let customFn = () => {}';
+        const signature3: string = 'var customFn = function(x) => {';
+        const signature4: string = 'function customFn(x, y){';
+        const signature5: string = 'function customFn(){';
+        const signature6: string = 'let x = function(param){';
 
         expect( extractFunctionName( signature ) ).to.equal( 'customFn' );
         expect( extractFunctionName( signature2 ) ).to.equal( 'customFn' );
@@ -85,22 +85,22 @@ suite('Doc String generation', () => {
     });
 
     test('correctly indents nested blocks', () => {
-        const oneSpaceSignature = ' fn(){';
-        const oneSpaceExpected =
+        const oneSpaceSignature: string = ' fn(){';
+        const oneSpaceExpected: string =
         ' /**\n' +
         '  * @function {function name}\n' +
         '  * @return {type} {description}\n' +
         '  */\n';
 
-        const threeSpaceIndentSignature = '   fn(){';
-        const threeSpaceIndentExpected =
+        const threeSpaceIndentSignature: string = '   fn(){';
+        const threeSpaceIndentExpected: string =
         '   /**\n' +
         '    * @function {function name}\n' +
         '    * @return {type} {description}\n' +
         '    */\n';
 
-        const fourSpaceIndentSignature = '    fn(){';
-        const fourSpaceIndentExpected =
+        const fourSpaceIndentSignature: string = '    fn(){';
+        const fourSpaceIndentExpected: string =
         '    /**\n' +
         '     * @function {function name}\n' +
         '     * @return {type} {description}\n' +
