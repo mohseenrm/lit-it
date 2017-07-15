2c7512a44cb99f203fd45e202c658aca581cadff
unknown <klamse47@gmail.com>
2017-07-14 22:35:47 -0500

    Removed unecessary stuff

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -1,8 +1,3 @@
-/* global suite, test */
-
-// The module 'assert' provides assertion methods from node
-import * as assert from 'assert';
-
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
