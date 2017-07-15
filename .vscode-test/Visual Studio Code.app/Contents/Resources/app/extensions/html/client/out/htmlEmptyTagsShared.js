3c5059563198acdde31887573c7f7dfbd2680756
mohseenrm <mohseenmukaddam6@gmail.com>
2017-07-14 23:40:59 -0700

    housekeeping

--- .vscode-test/Visual Studio Code.app/Contents/Resources/app/extensions/html/client/out/htmlEmptyTagsShared.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EMPTY_ELEMENTS = ['area', 'base', 'br', 'col', 'embed', 'hr', 'img', 'input', 'keygen', 'link', 'menuitem', 'meta', 'param', 'source', 'track', 'wbr'];
-//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/2648980a697a4c8fb5777dcfb2ab110cec8a2f58/extensions/html/client/out/htmlEmptyTagsShared.js.map
--- .vscode-test/Visual Studio Code.app/Contents/Resources/app/out/vs/workbench/services/search/node/searchApp.nls.ja.js
+++
@@ -1,5 +0,0 @@
-/*!--------------------------------------------------------
- * Copyright (C) Microsoft Corporation. All rights reserved.
- *--------------------------------------------------------*/
-define("vs/workbench/services/search/node/searchApp.nls.ja",{"vs/base/common/errorMessage":["{0}。エラー コード: {1}","アクセス許可が拒否されました (HTTP {0})","アクセス許可が拒否されました","{0} (HTTP {1}: {2})","{0} (HTTP {1})","不明な接続エラー ({0})","不明な接続エラーが発生しました。インターネット接続が切れたか、接続先のサーバーがオフラインです。","{0}: {1}","不明なエラーが発生しました。ログで詳細を確認してください。","システム エラーが発生しました ({0})","不明なエラーが発生しました。ログで詳細を確認してください。","{0} (合計 {1} エラー)","不明なエラーが発生しました。ログで詳細を確認してください。"],"vs/base/common/processes":["エラー: 実行可能ファイルの情報は、型 string のコマンドを定義する必要があります。","警告: isShellCommand は、型ブール値でなければなりません。値 {0} を無視します。","警告: 引数は、型 string[] でなければなりません。値 {0} を無視します。","警告: options.cwd は、型 string でなければなりません。値 {0} を無視します。"],"vs/base/node/processes":["UNC ドライブでシェル コマンドを実行できません。"]});
-//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/2648980a697a4c8fb5777dcfb2ab110cec8a2f58/core/vs/workbench/services/search/node/searchApp.nls.ja.js.map
